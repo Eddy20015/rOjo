6a3b52f8604143ae9c1df06f34b62d7bf0fc0c8b
Joyce Mai <joycemai.exe@gmail.com>
2023-04-21 00:02:48 -0700

    Basic Turn and Run Dolly
    changed the cutscene script into a base class for specific triggers to derive from
    removed hdrp package
    renamed several scripts to be more specific
    added a new scene to demo the dolly camera
    changed the temp dancer to greyscale besides the dress and added a spooky face

---
+++ Assets/Scripts/Joyce/CutsceneCameras/CutCameraTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutCameraTrigger : MonoBehaviour
+{
+    [Header("Cameras")]
+    [SerializeField] protected Camera mainCam;
+    [SerializeField] protected Animator cutAnim;
+
+    [Header("Switching")]
+    [SerializeField] protected CinemachineSwitcher switcher;
+    [SerializeField] protected int newCamIndex = 1;
+    [SerializeField] protected float transitionTime = 2f;
+
+    [Header("Cutscene")]
+    [SerializeField] protected AnimationClip cutSceneClip;
+    [SerializeField] protected string cutSceneName;
+
+    virtual protected void StartTrigger(){}
+    virtual protected void EndTrigger(){}
+
+    virtual protected IEnumerator OnCutSceneEnd()
+    {
+        yield return new WaitForSeconds(cutSceneClip.length);
+        // do something here lmao
+    }
+}
