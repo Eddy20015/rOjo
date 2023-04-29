814c4b9a2887accf01ad34fa21accf9aec4fd2ce
Joyce Mai <joycemai.exe@gmail.com>
2023-04-28 21:21:08 -0700

    Parallaxing images
    Parallax script that supports image looping
    renamed paralax script to parallax script
    added temporary backgrounds for testing
    added 2 new layers for sprite ordering, foreground and background, default will serve as the mid ground

--- Assets/Scripts/Joyce/ParallaxScript.cs
+++ Assets/Scripts/Joyce/ParallaxScript.cs
@@ -6,29 +6,36 @@ public class ParallaxScript : MonoBehaviour
 {
     [Range(0f, 1f)]
     [Tooltip("0 means that it will basically not move at all and 1 means it will follow the camera exactly. Use large values for far things")]
-    [SerializeField] private float paralaxEffectX;
+    [SerializeField] private float parallaxEffectX;
     [Range(0f, 1f)]
-    [SerializeField] private float paralaxEffectY;
+    [SerializeField] private float parallaxEffectY;
 
     [SerializeField] GameObject cam;
-    [SerializeField] 
-    private float startX, startY;
+    [SerializeField] bool isLooping;
+
+    private float startX, startY, length;
 
 
     void Start()
     {
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
         startX = transform.position.x;
         startY = transform.position.y;
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float temp = (cam.transform.position.x * (1 - paralaxEffectX));
-        float distX = (cam.transform.position.x * paralaxEffectX);
-        float distY = (cam.transform.position.y * paralaxEffectY);
+        float distX = (cam.transform.position.x * parallaxEffectX);
+        float distY = (cam.transform.position.y * parallaxEffectY);
         transform.position = new Vector3(startX + distX, startY + distY, transform.position.z);
 
-        //if (temp > startX + length) startX += length;
-        //else if (temp < startX - length) startX -= length;
+        if (isLooping) LoopImage();
+    }
+
+    private void LoopImage()
+    {
+        float temp = (cam.transform.position.x * (1 - parallaxEffectX));
+        if (temp > startX + length) startX += length;
+        else if (temp < startX - length) startX -= length;
     }
 }
