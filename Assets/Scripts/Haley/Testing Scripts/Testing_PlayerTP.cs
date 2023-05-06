6550e3df1dd0227139fcddd37b8cb60b298c1977
Joyce Mai <joycemai.exe@gmail.com>
2023-05-06 16:39:44 -0700

    Running into a wall end of demo

--- Assets/Scripts/Haley/Testing Scripts/Testing_PlayerTP.cs
+++ Assets/Scripts/Haley/Testing Scripts/Testing_PlayerTP.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Testing_PlayerTP : MonoBehaviour
 {
-    [SerializeField] private GameObject spawnpoint;
-    
+    //[SerializeField] private GameObject spawnpoint;
+    [SerializeField] private GameObject deathUI;
+
+    private void Awake()
+    {
+        deathUI.SetActive(false);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        collision.gameObject.transform.position = spawnpoint.transform.position;
+        if(collision.gameObject.tag.Equals("Player"))
+        {
+            GameStateManager.Pause();
+            deathUI.SetActive(true);
+        }
+        //collision.gameObject.transform.position = spawnpoint.transform.position;
+    }
+
+    public void OnDeathButton()
+    {
+        GameStateManager.QuitToMainMenu();
     }
 }
