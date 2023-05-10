c04ad090e3fba461f16fb16dcde4325de92ee954
tim-kashani <tim@fireapples.com>
2023-05-09 18:17:46 -0700

    animation

--- Assets/Scripts/Tim/Spider.cs
+++ Assets/Scripts/Tim/Spider.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Spider : MonoBehaviour
 {
+    [SerializeField] Transform body;
+
+    [SerializeField] float move, rotate, moveSpeed, rotateSpeed;
+
+    float moveTime, rotateTime;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,12 @@ public class Spider : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        moveTime += Time.deltaTime * moveSpeed;
+        rotateTime += Time.deltaTime * rotateSpeed;
+
         transform.position += Input.GetAxis("Horizontal") * Time.deltaTime * Vector3.right;
+
+        body.transform.SetLocalPositionAndRotation(move * Mathf.Sin(moveTime) * Vector3.up,
+            Quaternion.Euler(rotate * Mathf.Sin(rotateTime) * Vector3.forward));
     }
 }
