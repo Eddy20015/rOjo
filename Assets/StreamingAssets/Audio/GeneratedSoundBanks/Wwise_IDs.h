a6796aab7337c7aaf24ec8924c58daf6301346e4
Haley Vlahos <horseslovehaley@gmail.com>
2023-05-13 00:15:27 -0700

    Dirt Steps & Music Fade in Game

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID FADE_OUT_2D_MUSIC = 3111061815U;
         static const AkUniqueID PLAY_2D_MUSIC = 1931726204U;
         static const AkUniqueID PLAY_3D_MUSIC = 2395092899U;
         static const AkUniqueID PLAY_DIRT_FOOTSTEPS = 1323035329U;
