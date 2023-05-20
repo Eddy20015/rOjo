43dd6ba778445c4460f6445e093403067fa8040d
Jason Pambuena <zxgnj@MacBook-Pro-6.local>
2023-05-20 15:36:38 -0700

    Merge from BuildBranch2

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,23 +13,35 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID FADE_OUT_2D_MUSIC = 3111061815U;
         static const AkUniqueID PLAY_2D_MUSIC = 1931726204U;
-        static const AkUniqueID PLAY_3D_MUSIC = 2395092899U;
+        static const AkUniqueID PLAY_BUTTON_SELECT = 2034692297U;
         static const AkUniqueID PLAY_DIRT_FOOTSTEPS = 1323035329U;
+        static const AkUniqueID PLAY_EYEBEAM_DEFAULT = 3227116378U;
+        static const AkUniqueID PLAY_EYEBEAM_INTENSIFIED = 2379313215U;
+        static const AkUniqueID PLAY_EYEBEAM_INTENSIFIED2 = 2142483231U;
         static const AkUniqueID PLAY_FOOTSTEPS = 3854155799U;
+        static const AkUniqueID PLAY_HORROR_TEST = 4172395845U;
         static const AkUniqueID PLAY_JUMP_LANDING = 3808321334U;
+        static const AkUniqueID PLAY_SPECIAL_CLICK = 1401754570U;
         static const AkUniqueID PLAY_TESTING_AUDIO = 2946897393U;
         static const AkUniqueID PLAY_WIND_IN_BRANCHES = 1771244861U;
         static const AkUniqueID STOP_2D_MUSIC = 2367458202U;
-        static const AkUniqueID STOP_3D_MUSIC = 3398073649U;
+        static const AkUniqueID STOP_ALL = 452547817U;
+        static const AkUniqueID STOP_EYEBEAM_DEFAULT = 2047366256U;
+        static const AkUniqueID STOP_EYEBEAM_INTENSIFIED = 3359382657U;
+        static const AkUniqueID STOP_EYEBEAM_INTENSIFIED2 = 3080773409U;
+        static const AkUniqueID STOP_HORROR_TEST = 2289315611U;
         static const AkUniqueID STOP_TESTING_AUDIO = 87439343U;
         static const AkUniqueID STOP_WIND_IN_BRANCHES = 3026825475U;
     } // namespace EVENTS
 
     namespace GAME_PARAMETERS
     {
+        static const AkUniqueID FLAMENCOMUSICVOLUME = 4015289221U;
         static const AkUniqueID MASTER_VOLUME = 4179668880U;
         static const AkUniqueID MUSIC_VOLUME = 1006694123U;
+        static const AkUniqueID PLAYER_HEALTH = 215992295U;
         static const AkUniqueID SFX_VOLUME = 1564184899U;
     } // namespace GAME_PARAMETERS
 
