4c76abc97121c27c3e44a47f7fc2691863f87013
DragonTamer13 <wynekids@yahoo.com>
2023-05-21 23:53:51 -0700

    theater added to forest

--- Assets/Scripts/Avery/Theatre Puzzle/TheatreLightButton.cs
+++ Assets/Scripts/Avery/Theatre Puzzle/TheatreLightButton.cs
@@ -8,7 +8,7 @@ public class TheatreLightButton : MonoBehaviour
     [SerializeField] TheatreLightPuzzleManager manager;
     int netChangeInActiveLights = 0;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Dancer"))
         {
