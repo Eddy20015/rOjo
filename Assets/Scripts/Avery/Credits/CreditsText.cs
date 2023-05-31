ac420cdf4d3069e8bc57ae5903af2f3b0f76501e
Avery Baker <averyjb@uci.edu>
2023-05-31 11:21:43 -0700

    updated credits scene

--- Assets/Scripts/Avery/Credits/CreditsText.cs
+++ Assets/Scripts/Avery/Credits/CreditsText.cs
@@ -8,18 +8,57 @@ public class CreditsText : MonoBehaviour
     [SerializeField] List<GameObject> text;
     [SerializeField] Transform destination;
     [SerializeField] float moveSpeed = 2f;
+    [SerializeField] GameObject blackScreen;
     int numOfTexts, currentIndex = 0;
+    float fadeSpeed = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
         GameStateManager.Cinematics();
         numOfTexts = text.Count;
+        StartCoroutine(FadeAway());
         StartCoroutine(MoveTextAcrossScreen(text[currentIndex]));
+    }
+
+    IEnumerator FadeIn()
+    {
+        SpriteRenderer spriteRenderer = blackScreen.GetComponent<SpriteRenderer>();
+        Color startColor = spriteRenderer.color;
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 1f);
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeSpeed)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / fadeSpeed;
+            spriteRenderer.color = Color.Lerp(startColor, endColor, t);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    IEnumerator FadeAway()
+    {
+        SpriteRenderer spriteRenderer = blackScreen.GetComponent<SpriteRenderer>();
+        Color startColor = spriteRenderer.color;
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 0f); // Fade to transparent
+
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeSpeed)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / fadeSpeed;
+            spriteRenderer.color = Color.Lerp(startColor, endColor, t);
+            yield return new WaitForEndOfFrame();
+        }
     }
 
     IEnumerator MoveTextAcrossScreen(GameObject o)
     {
+        if (currentIndex == 0) yield return new WaitForSeconds(fadeSpeed);
+
         while (o.transform.localPosition != destination.localPosition)
         {
             o.transform.localPosition = Vector3.MoveTowards(o.transform.localPosition, destination.localPosition, moveSpeed * Time.deltaTime);
@@ -30,6 +69,10 @@ public class CreditsText : MonoBehaviour
         if (currentIndex < numOfTexts)
             StartCoroutine(MoveTextAcrossScreen(text[currentIndex]));
         else
+        {
+            StartCoroutine(FadeIn());
+            yield return new WaitForSeconds(fadeSpeed + 1f);
             GameStateManager.MainMenu();
+        }
     }
 }
